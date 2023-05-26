0ad59a9b9cd71541f09d020fdd2c37d206603a87
Alejo <alejoczombos@gmail.com>
2023-05-25 21:35:27 -0300

    Lectura y escritura de Archivos

--- Programacion III/EjercicioArchivos/Form1.Designer.cs
+++ Programacion III/EjercicioArchivos/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonCargarArchivo = new System.Windows.Forms.Button();
+            this.buttonAgregarArchivo = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
@@ -44,54 +46,86 @@
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 87.33334F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.66667F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(700, 338);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // richTextBox1
             // 
             this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Location = new System.Drawing.Point(3, 2);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(794, 386);
+            this.richTextBox1.Size = new System.Drawing.Size(694, 291);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
             // tableLayoutPanel2
             // 
-            this.tableLayoutPanel2.ColumnCount = 3;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 261F));
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Controls.Add(this.buttonCargarArchivo, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonAgregarArchivo, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.button1, 0, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 395);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 297);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(794, 52);
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(694, 39);
             this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // buttonCargarArchivo
+            // 
+            this.buttonCargarArchivo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonCargarArchivo.Location = new System.Drawing.Point(349, 2);
+            this.buttonCargarArchivo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonCargarArchivo.Name = "buttonCargarArchivo";
+            this.buttonCargarArchivo.Size = new System.Drawing.Size(167, 35);
+            this.buttonCargarArchivo.TabIndex = 2;
+            this.buttonCargarArchivo.Text = "Cargar Archivo";
+            this.buttonCargarArchivo.UseVisualStyleBackColor = true;
+            this.buttonCargarArchivo.Click += new System.EventHandler(this.buttonCargarArchivo_Click);
+            // 
+            // buttonAgregarArchivo
+            // 
+            this.buttonAgregarArchivo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonAgregarArchivo.Location = new System.Drawing.Point(176, 2);
+            this.buttonAgregarArchivo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonAgregarArchivo.Name = "buttonAgregarArchivo";
+            this.buttonAgregarArchivo.Size = new System.Drawing.Size(167, 35);
+            this.buttonAgregarArchivo.TabIndex = 1;
+            this.buttonAgregarArchivo.Text = "Agregar a Archivo";
+            this.buttonAgregarArchivo.UseVisualStyleBackColor = true;
+            this.buttonAgregarArchivo.Click += new System.EventHandler(this.buttonAgregarArchivo_Click);
             // 
             // button1
             // 
             this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Location = new System.Drawing.Point(3, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(260, 46);
+            this.button1.Size = new System.Drawing.Size(167, 35);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Seleccionar Directorio";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 338);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.tableLayoutPanel1.ResumeLayout(false);
@@ -106,5 +140,7 @@
         private RichTextBox richTextBox1;
         private TableLayoutPanel tableLayoutPanel2;
         private Button button1;
+        private Button buttonCargarArchivo;
+        private Button buttonAgregarArchivo;
     }
 }
--- Programacion III/EjercicioArchivos/Form1.cs
+++ Programacion III/EjercicioArchivos/Form1.cs
@@ -1,10 +1,21 @@
+using System.Reflection;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-
+/*
+Dado un directorio ingresado por el usuario. (Vea los Dialogs)
+Obtenga una lista de todos los archivos en esa carpeta, incluyendo el tamaño de 
+cada archivo, el tamaño total y el tamaño relativo %.
+- Vuelque la salida en un TextBox
+- Si encuentra una subcarpeta, márquela en amarillo y omita los tamaños.
+ */
 namespace EjercicioArchivos
 {
     public partial class Form1 : Form
     {
+
+        string textoAMostrar = "";
+        DialogResult resultado;
+
         public Form1()
         {
             InitializeComponent();
@@ -12,33 +23,89 @@ namespace EjercicioArchivos
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var fileContent = string.Empty;
-            var filePath = string.Empty;
-            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            //Creo y muestro un Dialog para que el usuario seleccione un directorio
+            FolderBrowserDialog ventanaDirectorio = new FolderBrowserDialog();
+
+            resultado = ventanaDirectorio.ShowDialog();
+
+            //Compruebo que se selecciono un directorio
+            if (resultado != DialogResult.OK)
             {
-                DialogResult result = folderDialog.ShowDialog();
-
-                if (result == DialogResult.OK)
-                {
-                    string directorio = folderDialog.SelectedPath;
-
-                    DirectoryInfo infoDirectorio = new DirectoryInfo(directorio);
-                    FileInfo[] archivos = infoDirectorio.GetFiles("*", SearchOption.AllDirectories);
-
-                    long tamañoTotal = directorio.Length;
-
-                    foreach (FileInfo archivo in archivos)
-                    {
-                        long tamañoArchivo = archivo.Length;
-
-                        double porcentajeRelativo = (double)tamañoArchivo / tamañoTotal * 100;
-
-                        string informaciónArchivo = $"-{archivo.Name}\n -Tamaño: {tamañoArchivo} bytes \n-Porcentaje relativo: {porcentajeRelativo}%\n";
-
-                        richTextBox1.AppendText(informaciónArchivo + Environment.NewLine);
-                    }
-                }
+                return;
             }
+
+            //Creo un DirectoryInfo a partir de el path que selecciono el usuario
+            DirectoryInfo informacionDirectorio = new DirectoryInfo(ventanaDirectorio.SelectedPath);
+
+            //Agrego todos los archivos del directorio seleccionado en una lista de FileInfo
+            FileInfo[] listaArchivos = informacionDirectorio.GetFiles();
+
+            //Sumo los tamaños de cada archivo para obtener el tamaño total
+            long tamanioDirectorio = informacionDirectorio.GetFiles().Sum(x => x.Length);
+
+            //Itero y agrego al string a mostrar la informacion pertinente
+            foreach(var archivo in listaArchivos)
+            {
+                textoAMostrar += $"{archivo.Name} {archivo.Length} {(archivo.Length * 100) / tamanioDirectorio}%\n";
+            }
+
+            //Imprimo en el richTextBox el texto
+            richTextBox1.Text = textoAMostrar;
+        }
+
+        private void buttonAgregarArchivo_Click(object sender, EventArgs e)
+        {
+            if(resultado != DialogResult.OK)
+            {
+                return;
+            }
+
+            //Obtengo la ruta relativa de la solucion con el nombre del archivo
+            string nombreArchivo = "InformacionDirectorio.txt";
+            string rutaArchivo = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), nombreArchivo);
+
+            //Creo el FileInfo con la ruta anterior
+            FileInfo f = new FileInfo(rutaArchivo);
+
+            //Creo el FileStram con la funcion de crear o sobreescribir
+            FileStream fs = f.Create();
+
+            //Creo un StreamWriter y escribo el texto resultante del directorio seleccionado por el usuario en el archivo
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                sw.WriteLine(textoAMostrar);
+            }
+
+            //Muestro al usuario un mensaje
+            MessageBox.Show("Archivo guardado con exito");
+
+            // cierro el FileStream
+            fs.Close();
+        }
+
+        private void buttonCargarArchivo_Click(object sender, EventArgs e)
+        {
+            //Obtengo la ruta relativa de la solucion con el nombre del archivo
+            string nombreArchivo = "InformacionDirectorio.txt";
+            string rutaArchivo = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), nombreArchivo);
+
+            //Creo el FileInfo con la ruta anterior
+            FileInfo f = new FileInfo(rutaArchivo);
+
+            //Creo el FileStram con la funcion de abrir y leer
+            FileStream fs = f.OpenRead();
+
+            //Creo el StreamReader
+            StreamReader sr = new StreamReader(fs);
+
+            //Creo un string y le asigno todo el texto del archivo
+            string text = sr.ReadToEnd();
+
+            //Muestro por pantalla el texto del archivo
+            richTextBox1.Text = textoAMostrar;
+
+            //Cierro el StreamReader
+            sr.Close();
         }
     }
 }
